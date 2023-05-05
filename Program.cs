16f73f4cf0c4384f796d16b8f6f9cb3a90f214ac
keszki <keszki@outlook.com>
2023-05-05 14:16:07 +0200

    Calculator should be working
    For now

--- Program.cs
+++ Program.cs
@@ -1,34 +1,71 @@
-﻿namespace Calculator
+namespace Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string read = "error";
-            int x = 0;
-            int y = 0;
+            int loop = 1;
+            string read;
+            int x;
+            int y;
+            int result;
 
             Console.WriteLine("Welcome to my calculator");
             Console.WriteLine("Current possible actions are:");
-            Console.WriteLine("1.Addiction");
-            Console.Write("I want to test the: ");
-            read = Console.ReadLine();
-
-            Console.Write("Write first number: ");
-            x = int.Parse(Console.ReadLine());
-            Console.Write("Write second number: ");
-            y = int.Parse(Console.ReadLine());
-
-            if (read == "error")
+            Console.WriteLine("1.Addiction, 2.Subtraction, 3.Multiplication, 4.Division");
+            while (loop > 0)
             {
-                Console.WriteLine("!! !! ERROR !! !!");
-            }
-            else if (read == "1" || read == "Addiction" || read == "addiction")
-            {
-                Console.WriteLine("OK Thanks");
+                Console.Write("I want to test the: ");
+                read = Console.ReadLine();
+
+                Console.Write("Write the first number: ");
+                int.TryParse(Console.ReadLine(), out x);
+                Console.Write("Write the second number: ");
+                int.TryParse(Console.ReadLine(), out y);
+
+                if (read == "1" || read == "+" || read.ToLower().Contains("add"))
+                {
+                    result = x + y;
+
+                    Console.WriteLine("Your result is " + result);
+                }
+                else if (read == "2" || read == "-" || read.ToLower().Contains("sub"))
+                {
+                    result = x - y;
+
+                    Console.WriteLine("Your result is " + result);
+                }
+                else if (read == "3" || read == "*" || read.ToLower().Contains("mul"))
+                {
+                    result = x * y;
+
+                    Console.WriteLine("Your result is " + result);
+                }
+                else if (read == "4" || read == "/" || read.ToLower().Contains("div"))
+                {
+                    result = x / y;
+
+                    Console.WriteLine("Your result is " + result);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+                Console.WriteLine("Do you want to use it again?");
+                string LOOP = Console.ReadLine();
+                if (LOOP.ToLower().Contains("y"))
+                {
+                    loop++;
+                }
+                else
+                {
+                    Console.WriteLine("You've done " + loop + " loops! :D");
+
+                    loop = 0;
+                }
             }
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
